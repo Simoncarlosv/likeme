e106de19b9389be345a1b46440d2cbba55fb1b3d
Simoncarlosv <simoncarlosv@gmail.com>
2024-12-10 14:21:00 -0300

    modificacion con middleware para errores

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const routes = require('./routes/routes')
 const cors = require('cors')
+const { errorMiddleware } = require('./middlewares/errorMiddlewares')
 //const cors = require('cors');
 
 const app = express()
@@ -16,5 +17,7 @@ app.use(morgan('dev'))
 app.use('/', routes)
 
 
+// Utlizamos nuestro middleware de manejador de errores, y debe ir AQUI al final, por el parámetro "next", que va pasando el proceso por los otros pasos para ir detectando los errores.
+app.use(errorMiddleware)
 
 module.exports = app
--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -4,47 +4,50 @@ const { crearPost, obtenerPosts, actualizarPost, eliminarPost } = require('../mo
 
 
 // Obtener todos los posts
-const getPosts = async (req, res) => {
+const getPosts = async (req, res, next) => {
     try {
         const posts = await obtenerPosts();
         res.json(posts);
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error al obtener los posts' });
+        //console.error(error);
+        //res.status(500).json({ error: 'Error al obtener los posts' });
+        next(error)
     }
 };
 
 // Crear un nuevo post / los controladores ahora seran con ASYNC porque nos debemos cominicar con la base de datos
-const createPost = async (req, res) => {
+const createPost = async (req, res, next) => {
     try {
         const { titulo, url, descripcion } = req.body;
         const likes = 0; // Valor inicial de likes
         const nuevoPost = await crearPost(titulo, url, descripcion, likes);
         res.status(201).json({ message: 'Post creado con éxito', post: nuevoPost.rows[0] });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error al crear el post' });
+        //console.error(error);
+        //res.status(500).json({ error: 'Error al crear el post' });
+        next(error)
     }
 };
 
 // Actualizar un post
-const updatePost = async (req, res) => {
+const updatePost = async (req, res, next) => {
     try {
         const { id } = req.params;
         const { titulo, url, descripcion } = req.body;
         const postActualizado = await actualizarPost(id, titulo, url, descripcion);
         if (!postActualizado) {
-            return res.status(404).json({ error: 'Post no encontrado' });
+            return res.status(404).json({ error: 'Post no encontrado' });           
         }
         res.json({ message: 'Post actualizado con éxito', post: postActualizado });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error al actualizar el post' });
+        //console.error(error);
+        //res.status(500).json({ error: 'Error al actualizar el post' });
+        next(error)
     }
 };
 
 // Eliminar un post
-const deletePost = async (req, res) => {
+const deletePost = async (req, res, next) => {
     try {
         const { id } = req.params;
         const postEliminado = await eliminarPost(id);
@@ -53,8 +56,9 @@ const deletePost = async (req, res) => {
         }
         res.json({ message: 'Post eliminado con éxito', post: postEliminado });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error al eliminar el post' });
+       // console.error(error);
+        //res.status(500).json({ error: 'Error al eliminar el post' });
+        next(error)
     }
 };
 
